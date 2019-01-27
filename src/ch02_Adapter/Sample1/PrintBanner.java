c949f4644fdd6d0191a748c04c064e9bafed6ce7
yinm <yinm.cminor9@gmail.com>
2019-01-27 09:56:10 +0900

    Check Adapter pattern

---
+++ src/ch02_Adapter/Sample1/PrintBanner.java
@@ -0,0 +1,13 @@
+package ch02_Adapter.Sample1;
+
+public class PrintBanner extends Banner implements Print {
+    public PrintBanner(String string) {
+        super(string);
+    }
+    public void printWeak() {
+        showWithParen();
+    }
+    public void printStrong() {
+        showWithAster();
+    }
+}
