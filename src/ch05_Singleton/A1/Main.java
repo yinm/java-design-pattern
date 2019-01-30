063cde498bc48fedae9faf0fdbb15895aa6843fe
yinm <yinm.cminor9@gmail.com>
2019-01-30 09:39:42 +0900

    Check

--- src/ch05_Singleton/A1/Main.java
+++ src/ch05_Singleton/A1/Main.java
@@ -2,8 +2,8 @@ package ch05_Singleton.A1;
 
 public class Main {
     public static void main(String[] args) {
-        TicketMaker obj1 = new TicketMaker();
-        TicketMaker obj2 = new TicketMaker();
+        TicketMaker obj1 = TicketMaker.getInstance();
+        TicketMaker obj2 = TicketMaker.getInstance();
 
         System.out.println("obj1: " + obj1.getNextTicketMaker());
         System.out.println("obj1: " + obj1.getNextTicketMaker());
--- src/ch05_Singleton/A1/TicketMaker.java
+++ src/ch05_Singleton/A1/TicketMaker.java
@@ -1,7 +1,14 @@
 package ch05_Singleton.A1;
 
 public class TicketMaker {
+    private static TicketMaker ticketMaker = new TicketMaker();
     private int ticket = 1000;
+    private TicketMaker() {
+        System.out.println("インスタンスを生成しました。");
+    }
+    public static TicketMaker getInstance() {
+        return ticketMaker;
+    }
     public int getNextTicketMaker() {
         return ticket++;
     }
