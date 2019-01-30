6edc22899ca610f7c883e4d25208cea90ac5c64a
yinm <yinm.cminor9@gmail.com>
2019-01-30 10:03:01 +0900

    Check

---
+++ src/ch05_Singleton/A3_2/Main.java
@@ -0,0 +1,17 @@
+package ch05_Singleton.A3_2;
+
+public class Main extends Thread {
+    public static void main(String[] args) {
+        new Main("A").start();
+        new Main("B").start();
+        new Main("C").start();
+        System.out.println("End.");
+    }
+    public void run() {
+        Singleton obj = Singleton.getInstance();
+        System.out.println(getName() + ": obj = " + obj);
+    }
+    public Main(String name) {
+        super(name);
+    }
+}
