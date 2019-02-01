a9e0e2fe8fad5554afa3ea8db1eae3071f592ce9
yinm <yinm.cminor9@gmail.com>
2019-02-01 10:06:03 +0900

    Check

---
+++ src/ch07_Builder/A2/HTMLBuilder.java
@@ -0,0 +1,35 @@
+package ch07_Builder.A2;
+
+import java.io.*;
+
+public class HTMLBuilder extends Builder {
+    private String filename;
+    private PrintWriter writer;
+    public void buildTitle(String title) {
+        filename = title + ".html";
+        try {
+            writer = new PrintWriter(new FileWriter(filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        writer.println("<html><head><title>" + title + "</title></head><body>");
+        writer.println("<h1>" + title + "</h1>");
+    }
+    public void buildString(String str) {
+        writer.println("<p>" + str + "</p>");
+    }
+    public void buildItems(String[] items) {
+        writer.println("<ul>");
+        for (int i = 0; i < items.length; i++) {
+            writer.println("<li>" + items[i] + "</li>");
+        }
+        writer.println("</ul>");
+    }
+    public void buildDone() {
+        writer.println("</body></html>");
+        writer.close();
+    }
+    public String getResult() {
+        return filename;
+    }
+}
---
+++ src/ch07_Builder/A2/TextBuilder.java
@@ -0,0 +1,26 @@
+package ch07_Builder.A2;
+
+public class TextBuilder extends Builder {
+    private StringBuffer buffer = new StringBuffer();
+    public void buildTitle(String title) {
+        buffer.append("===========================\n");
+        buffer.append("『" + title + "』\n");
+        buffer.append("\n");
+    }
+    public void buildString(String str) {
+        buffer.append('■' + str + "\n");
+        buffer.append("\n");
+    }
+    public void buildItems(String[] items) {
+        for (int i = 0; i < items.length; i++) {
+            buffer.append(" ・" + items[i] + "\n");
+        }
+        buffer.append("\n");
+    }
+    public void buildDone() {
+        buffer.append("===========================\n");
+    }
+    public String getResult() {
+        return buffer.toString();
+    }
+}
